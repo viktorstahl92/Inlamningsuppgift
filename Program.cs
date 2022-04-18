d4d6b8b2f0f442ac19f15c04de9b5e5fddad012f
viktorstahl92 <info@viktorstahl.com>
2022-04-18 21:41:04 +0200

    Cors added

--- Program.cs
+++ Program.cs
@@ -67,6 +67,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthentication();
 
 app.UseAuthorization();
